bd2e93016dd7d1361ed58f1f9618f56bdb5cdc66
markcool17 <45372340+markcool17@users.noreply.github.com>
2021-05-27 17:38:17 +0300

    Добавьте файлы проекта.

---
+++ ServerLayer/DataObtaining/AppDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using ServerLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerLayer.DataObtaining
+{
+    public class AppDataContext:IdentityDbContext<AppUser>
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options)
+            : base(options)
+        {
+        }
+    }
+}
